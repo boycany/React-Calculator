3e026ef746ff39ba01f0fa2a04d434f79cd3cc03
boycany <yurayurateiko@gmail.com>
2022-02-19 16:32:19 +0800

    mobile work fine

--- src/App.js
+++ src/App.js
@@ -39,36 +39,38 @@ const App = () => {
   const buttonColor = '#f2a33c'
 
   return (
-    <div className="App">
-      <div className="calc-wrapper">
-        <Input text={text} result={result}/>
-        <div className="row">
-          <Button symbol='Clear' handleClick={resetInput} color='#a897f9'/>
-          <Button symbol='Back' handleClick={undoInput} color='#a897f9'/>
-        </div>
-        <div className="row">
-          <Button symbol='7' handleClick={addtoText}/>
-          <Button symbol='8' handleClick={addtoText}/>
-          <Button symbol='9' handleClick={addtoText}/>
-          <Button symbol='/' handleClick={addtoText} color={buttonColor}/>
-        </div>
-        <div className="row">
-          <Button symbol='4' handleClick={addtoText}/>
-          <Button symbol='5' handleClick={addtoText}/>
-          <Button symbol='6' handleClick={addtoText}/>
-          <Button symbol='*' handleClick={addtoText} color={buttonColor}/>
-        </div>
-        <div className="row">
-          <Button symbol='1' handleClick={addtoText}/>
-          <Button symbol='2' handleClick={addtoText}/>
-          <Button symbol='3' handleClick={addtoText}/>
-          <Button symbol='-' handleClick={addtoText} color={buttonColor}/>
-        </div>
-        <div className="row">
-          <Button symbol='0' handleClick={addtoText}/>
-          <Button symbol='.' handleClick={addtoText}/>
-          <Button symbol='=' handleClick={calculateResult}/>
-          <Button symbol='+' handleClick={addtoText} color={buttonColor}/>
+    <div className='container'>
+      <div className="App">
+        <div className="calc-wrapper">
+          <Input text={text} result={result}/>
+          <div className="row">
+            <Button symbol='Clear' handleClick={resetInput} color='#a897f9'/>
+            <Button symbol='Back' handleClick={undoInput} color='#a897f9'/>
+          </div>
+          <div className="row">
+            <Button symbol='7' handleClick={addtoText}/>
+            <Button symbol='8' handleClick={addtoText}/>
+            <Button symbol='9' handleClick={addtoText}/>
+            <Button symbol='/' handleClick={addtoText} color={buttonColor}/>
+          </div>
+          <div className="row">
+            <Button symbol='4' handleClick={addtoText}/>
+            <Button symbol='5' handleClick={addtoText}/>
+            <Button symbol='6' handleClick={addtoText}/>
+            <Button symbol='*' handleClick={addtoText} color={buttonColor}/>
+          </div>
+          <div className="row">
+            <Button symbol='1' handleClick={addtoText}/>
+            <Button symbol='2' handleClick={addtoText}/>
+            <Button symbol='3' handleClick={addtoText}/>
+            <Button symbol='-' handleClick={addtoText} color={buttonColor}/>
+          </div>
+          <div className="row">
+            <Button symbol='0' handleClick={addtoText}/>
+            <Button symbol='.' handleClick={addtoText}/>
+            <Button symbol='=' handleClick={calculateResult}/>
+            <Button symbol='+' handleClick={addtoText} color={buttonColor}/>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -13,13 +13,6 @@ const Input = ({text, result}) => {
                 value={text} 
                 className='text' 
                 readOnly />
-            
-            {/* <div className='result'>
-                <h1>{result}</h1>
-            </div>
-            <div className='text'>
-                <h3>{text}</h3>
-            </div> */}
         </div>
     
     )
